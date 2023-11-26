2ef05f04e328975217d73f1d0524346635a30874
Jefferson-Lima-Santos <jefferson.santos@sebsa.com.br>
2023-11-26 19:01:36 -0300

    Novos objetos no end-point

--- index.py
+++ index.py
@@ -36,7 +36,6 @@ def process_file_content(numFuncionarios, minFuncionarios, numFolgas):
     for l in range(QuantidadeLinhas):
         dias_folga = [int(dia)-1 for dia in numFolgas[l]]
         numeFolga.append(dias_folga)
-        print(numeFolga[l])
         
     modelo = gp.Model()  # inicializa o modelo
 
@@ -50,6 +49,7 @@ def process_file_content(numFuncionarios, minFuncionarios, numFolgas):
     # modelo.setObjective(sum(x[i,j] for i in range(n) for j in numeFolga[i]), sense = gp.GRB.MINIMIZE)
     modelo.setObjective(z, sense=gp.GRB.MINIMIZE)
 
+    numRestricoesManuais = 0
     # RESTRIÇÕES
     # Cada funcionário deve ter 5 folgas durante o mês, soma dos dias trabalhados
     # de cada funcionário deve ser exatamente igual os dias do mês - 5 folgas de direito
@@ -57,30 +57,36 @@ def process_file_content(numFuncionarios, minFuncionarios, numFolgas):
         sum(x[i,j] for j in numeFolga[i]) <= z
         for i in range(n)
     )
+    numRestricoesManuais += 1
 
 
     c1 = modelo.addConstrs(sum(x[i, j] for j in range(m)) == m - NF for i in range(n))
-
+    numRestricoesManuais += 1
+    
     # A equipe não pode operar com menos de x funcionários (definido contratualmente) em um dia
     c2 = modelo.addConstrs(
         sum(x[i, j] for i in range(n)) >= minFuncionarios for j in range(m)
     )
-
+    numRestricoesManuais += 1
+    
     # Nenhum funcionário pode trabalhar por 6 dias seguidos sem folga
     c3 = modelo.addConstrs(
         sum(x[i, j] for j in range(k, k + 6)) <= 5
         for k in range(m - 5)
         for i in range(n)
     )
+    numRestricoesManuais += 1
 
     # Pelo menos uma folga deve ocorrer em um domingo ou feriado
     c4 = modelo.addConstrs(sum(x[i, j] for j in H) == 6 for i in range(n))
+    numRestricoesManuais += 1
 
     # Pelo menos duas folgas deve ocorrer dentro dos dias escolhidos pelos funcionários
     # c5 = modelo.addConstrs(
     #     sum(x[i,j] for j in numeFolga[i]) >= 2
     #     for i in range (n)
     # )
+    # numRestricoesManuais += 1
 
     # Suprimindo console output
     modelo.setParam("OutputFlag", 0)
@@ -93,22 +99,28 @@ def process_file_content(numFuncionarios, minFuncionarios, numFolgas):
     # print("Status = ", status)
     app = Flask(__name__)
     numDias = (len(numeFolga) * 4) - int(modelo.objVal)
+    diasTotais = len(numeFolga) * 4
+    numeFolgasEscolhidas = []
+    for i in range(n):
+        numeFolgasEscolhidas.append({"id": i + 1, "value": numeFolga[i]})
     obj = []
     if status == gp.GRB.OPTIMAL:
-        print("Valor ótimo = {}".format(modelo.objVal))
-        print("Dias atendidos: {}\n".format(numDias))
-        print("Escala de Funcionários para o mês de novembro de 2023 (Dias Trabalhados):")
+        obj.append({"ValorOtimo": modelo.objVal})
+        obj.append({"DiasAtendidos": numDias})
+        obj.append({"DiasNaoAtendidos": diasTotais - numDias})
+        obj.append({"QuantidadeRestricoes": numRestricoesManuais})
+        folgasPreferidas = []
+        # Escala de funcionários para o mês de novembro de 2023
         for i in range(n):
             escala = [j + 1 for j in range(m) if x[i, j].x == 1]
-            print(f"Func {i + 1}: {escala}")
-        print("\n\nEscala de Funcionários para o mês de novembro de 2023 (Folgas):")
         for i in range(n):
             escala = [j + 1 for j in range(m) if x[i, j].x == 0]
-            obj.append({"id": i + 1, "value": escala})
-            print(f"Func {i + 1}: {escala}")
+            folgasPreferidas.append({"id": i + 1, "value": escala})
+        obj.append({"DiasCalculados": folgasPreferidas})
+        obj.append({"DiasEscolhidos": numeFolgasEscolhidas})
         return json.dumps(obj)
     else:
-        print("Problema não resolvido")
+        return "Problema não resolvido"
 
 
 if __name__ == "__main__":
